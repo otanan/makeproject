73005016f915dfe34c6f3e061c25cc3e29511b37
Jonathan Delgado <jonathandelgado0327@gmail.com>
2023-03-15 21:04:30 -0700

    push with yaml refactor for updating file contents with template files.

--- makeproject.py
+++ makeproject.py
@@ -17,14 +17,90 @@ from itermlink.tools.typing_filter import launch as launch_filter
 import itermlink
 #======================== Fields ========================#
 __version__ = 0.10
+FILE_KEY = '$' # key for file replacements
+#======================== Readers ========================#
+
+def template_folder():
+    return Path(__file__).parent / 'templates'
+
+
+def get_template_contents(contents_name):
+    """ Gets the contents of a template file. """
+    with open(template_folder() / contents_name, 'r') as f:
+        return f.read()
+
+
+def get_struct_string(project_type, name):
+    """ Gets the YAML struct string to generate the project. """
+    structs_folder = Path(__file__).parent / 'project_structs'
+    with open(structs_folder / (project_type + '.yaml'), 'r') as f:
+        struct_string = f.read()
+
+    # Direct replacements
+    return struct_string.replace("{name}", format_name(name))
+
+#======================== Writers ========================#
+
+def update_from_template(path):
+    """ Update the file using the corresponding template. """
+    template_name = str(path.name)[len(FILE_KEY):]
+    with open(path, 'w') as f:
+        f.write(get_template_contents(template_name))
+
+
 #======================== Helper ========================#
+
+def format_name(name):
+    """ Format a general name to a standard filename convention. Converts something like "Real Analysis" to "real_analysis". """
+    return name.lower().replace(' ', '_')
+
+
+def remove_key_in_fname(path):
+    """ Rename the file to remove the FILE_KEY in its name. """
+    name = path.parent / str(path.name)[len(FILE_KEY):]
+    path.rename(name)
+
+
+def parse_struct_tree(dst):
+    """ Runs through the constructed structure tree and replaces the file contents appropriately. """
+    # Walk through project's directories
+    for root, dirs, files in os.walk(dst):
+        for file in files:
+            # Check whether this file is intended to be updated
+            if FILE_KEY in file:
+                # Key found in path, get the relative path
+                file = (Path(root) / file).relative_to(dst)
+                print(f'Detected key: [success]{file.name}[/]... updating contents...')
+                update_from_template(file)
+                remove_key_in_fname(file)
+
+
+#======================== Messages ========================#
+
 
 def welcome():
     """ Provides a welcome screen. """
     print(f'Project Generator - v:{__version__}')
 
 
-def get_dst():
+def declare_project_generation(project_type, dst):
+    """ Prints a header for which type of project is being generated and the location. """
+    print(f'Making [emph]{project_type}[/] Project at: [success]{dst}[/].')
+
+
+#======================== Queries ========================#
+
+def get_project_type(projects):
+    """ Query the user for the project type. """
+    return launch_filter(options=list(projects.keys()))
+
+
+def get_project_name():
+    """ Query for the project name. """
+    return ask('[emph]Project name')
+
+
+def get_destination():
     """ Gets the destination folder for any copying. """
     dst = Path.cwd()
     # Check whether cwd is desired destination
@@ -35,128 +111,44 @@ def get_dst():
     return dst
 
 
-def declare_generation(project_type, dst):
-    """ Prints a header for which type of project is being generated and the location. """
-    print(f'Making [emph]{project_type}[/] Project at: [success]{dst}[/].')
-
-
-def template_folder():
-    return Path(__file__).parent / 'templates'
-
-
-def safe_copy_tree(src, dst, ignore=None):
-    """ Wrapper for shutil.copytree to check for path existence and prompt to continue. """
-    if not isinstance(dst, Path): dst = Path(dst)
-
-    if dst.exists():
-        # Folder exists, prompt to continue
-        if not confirm('Folder exists, overwrite?', default=False):
-            # Quit the program
-            sys.exit()
-
-        # Delete the folder
-        print('Deleting existing directory...')
-        shutil.rmtree(dst)
-
-    if ignore is not None: ignore = shutil.ignore_patterns(*ignore)
-    shutil.copytree(src, dst, ignore=ignore)
-
-
-def format_name(name):
-    """ Format a general name to a standard filename convention. Converts something like "Real Analysis" to "real_analysis". """
-    return name.lower().replace(' ', '_')
-
-
 #======================== Project Generation ========================#
 
-
-def master(dst):
-    """ Generate master project. """
-    print(f'Making master project at: {dst}')
-    src_folder = template_folder()
-    
-    name = ask('[emph]Master Project name')
-
-    master_folder = dst / format_name(name)
-    safe_copy_tree(src_folder, master_folder)
-
-    print('Master project generated.')
-    itermlink.cd(master_folder)
+def generate_project(name, dst, generator):
+    """ Main loop function for generating the project. """
+    declare_project_generation(name, dst)
+    # Run the generator at this destination
+    generator(name, dst)
 
 
-def project(dst): pass
+def exercises(name, dst):
+    struct_string = get_struct_string('exercises', name)
+    try:
+        Filemaker(dst, struct_string)
+    except FileExistsError as e:
+        if confirm(
+            'Project exists... [red]delete[/] and continue?', default=False
+        ):
+            shutil.rmtree(dst / 'exercises')
+            # Run again
+            exercises(name, dst)
+
+    parse_struct_tree(dst)
 
 
-def scripts(dst):
-    """ Generate scripts project. """
-    print(f'Making Scripts project at: {dst}')
-    folder = template_folder() / 'project-name/scripts'
-    shutil.copytree(folder, dst / 'scripts')
-
-    # Get iTerm session and change directory
-    itermlink.cd(dst / 'scripts')
-
-
-def package(dst): pass
-
-
-def treatise(dst):
-    """ Generate scripts project. """
-    print(f'Making Treatise project at: {dst}')
-    src_folder = template_folder() / 'treatise'
-    
-    name = ask('[emph]Project name')
-
-    # Copy the treatise template while ignoring the .gitkeep files
-    treatise_folder = dst / 'treatise'
-    safe_copy_tree(src_folder, treatise_folder, ignore=['*.gitkeep'])
-
-    with status('Cleaning files...'):
-        (treatise_folder / 'subject.tex').rename(
-            treatise_folder / f'{name.lower()}.tex'
-        )
-
-        (treatise_folder / 'README.md').unlink()
-
-    print('Treatise generated.')
-
-
-def exercises(dst):
-    src_folder = template_folder() / 'exercises'    
-    
-    name = ask('[emph]Project name')
-
-    project_folder = dst / 'exercises'
-    safe_copy_tree(src_folder, project_folder, ignore=['*.gitkeep'])
-
-    with status('Cleaning files...'):
-        (project_folder / 'exercises-subject.tex').rename(
-            project_folder / f'exercises-{format_name(name)}.tex'
-        )
-
-        # Delete the README file
-        (project_folder / 'README.md').unlink(missing_ok=True)
-
-    print(f'Exercises generated: [emph]{dst}[/].')
-
-
-
-def testyaml(dst):
-    with open(template_folder().parent / 'yaml_structures/exercises.yaml', 'r') as f:
-        yamlfile = f.read()
-    
-    Filemaker(Path.cwd(), yamlfile)
+def test():
+    exercises('Real Analysis', Path.cwd())
+    exit()
 
 #======================== Entry ========================#
 
 def main():
     #------------- Setup -------------#
+    # Dictionary with types of projects and their generators
     projects = {
         #--- General Projects ---#
         # 'master': master,
         # 'project': ('Project', project),
         'Exercises': exercises,
-        'YAML Test': testyaml,
         # 'scripts': scripts,
         # 'package': package,
         # 'treatise': treatise,
@@ -166,15 +158,17 @@ def main():
     }
 
     #------------- Main logic -------------#
+    test()
+
     welcome()
 
-    project_type = launch_filter(options=list(projects.keys()))
-
+    project_type = get_project_type(projects)
+    name = get_project_name()
     # Potential output directory
-    dst = get_dst()
-    declare_generation(project_type, dst)
-    # Run the generator at this destination
-    projects[project_type](dst)
+    dst = get_destination()
+    # Run the generation functions
+    generate_project(name, dst, projects[project_type])
+    print(f'Project [emph]{name}[/] generated: [success]{dst}[/].')
 
     
 
